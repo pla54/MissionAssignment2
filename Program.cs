841cdf559565feca29c58969b4d06960caf08699
pla54 <113071547+pla54@users.noreply.github.com>
2024-01-24 01:15:27 -0700

    Comments

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //Welcoming the user to the program and getting user input for the number of rolls
         System.Console.WriteLine("Welcome to the dice throwing simulator!");
         System.Console.WriteLine("How many dice rolls would you like to simulate?");
 
         int numrolls = 0;
         numrolls = int.Parse(System.Console.ReadLine());
 
+        //Creating an object of the diceroller and then rolling the dice with the user information
         RollDice diceRoller = new RollDice();
 
         int[] resultsArray = diceRoller.rolling(numrolls);
@@ -18,6 +20,7 @@ internal class Program
         System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
         System.Console.WriteLine("Total number of rolls = " + numrolls);
 
+        //Printing out the information as according to the ruberic and the percentage of each roll
         for(int i = 2; i < 13; i++)
         {
             int percentage = (int)Math.Round((resultsArray[i] * 100.0) / numrolls);
--- RollDice.cs
+++ RollDice.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace MissionAssignment2
 {
+ //Creating class to roll the dice that returns the array of dice rolls in each respective position
     internal class RollDice
     {
         public int[] rolling(int numberOfRolls)
@@ -13,6 +14,7 @@ namespace MissionAssignment2
             int[] results = new int[13];
             Random r = new Random();
 
+            //Rolling the two random dies according to how many rolls to do. This is done with the Random library
             for (int i = 0; i < numberOfRolls; i++)
             {
                 int dice1 = r.Next(1, 7);
