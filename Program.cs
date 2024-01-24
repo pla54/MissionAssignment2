3fda196c0d6b15edf7a9eb7d8c81395e909e0f2b
pla54 <113071547+pla54@users.noreply.github.com>
2024-01-24 01:09:26 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using MissionAssignment2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        int numrolls = 0;
+        numrolls = int.Parse(System.Console.ReadLine());
+
+        RollDice diceRoller = new RollDice();
+
+        int[] resultsArray = diceRoller.rolling(numrolls);
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + numrolls);
+
+        for(int i = 2; i < 13; i++)
+        {
+            int percentage = (int)Math.Round((resultsArray[i] * 100.0) / numrolls);
+
+            Console.WriteLine(i + ": " + new string('*', percentage));
+        }
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionAssignment2
+{
+    internal class RollDice
+    {
+        public int[] rolling(int numberOfRolls)
+        {
+            int[] results = new int[13];
+            Random r = new Random();
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int dice1 = r.Next(1, 7);
+                int dice2 = r.Next(1, 7);
+                int sum = dice1 + dice2;
+
+                results[sum]++;
+            }
+
+            return results;
+        }
+    }
+}
